67ee80b964c8f26550f583a21ff1fafc6f709250
Jiwoo Jeong <realrv0801@gmail.com>
2025-09-18 15:46:47 -0400

    unused files removed

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./components/global.css";
 
-import Index from "./pages/Index";
 import WebLanding from "./pages/WebLanding";
 import MobileLanding from "./pages/MobileLanding";
 import Loading from "./pages/Loading";
-import Recommendations from "./pages/Recommendations";
-import CatalogSheet from "./pages/CatalogSheet"; // ✅ 추가
 import NotFound from "./pages/NotFound";
 import Onboarding from "./pages/OnBoarding";
 import MobileIndex from "./pages/mobileIndex";
@@ -33,10 +30,7 @@ const App = () => (
           <Route path="/" element={isMobile ? <MobileLanding /> : <WebLanding />} />
           <Route path="/landing" element={<MobileLanding />} />
           <Route path="/MobileLanding" element={<MobileLanding />} /> {/* alias for back button */}
-          <Route path="/index" element={<Index />} />
           <Route path="/loading" element={<Loading />} />
-          <Route path="/recommendations" element={<Recommendations />} />
-          <Route path="/catalog-sheet" element={<CatalogSheet />} /> {/* ✅ 현재 등록된 추천작(시트) */}
           <Route path="/onboarding" element={<Onboarding />} />
           <Route path="/mobile-index" element={<MobileIndex />} />
           <Route path="/mobile-recom" element={<MobileRecom />} />
